e388cefe287cb34aab99b5ef8c6e51bb4e4785d4
Soptq <32592090+Soptq@users.noreply.github.com>
2022-10-15 11:05:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ SETUP_REQUIRED = [
 REQUIRED = [
     f'aim-ui=={__version__}',
     'aimrecords==0.0.7',
-    'aimrocks==0.2.1',
+    'aimrocks==0.2.2a2',
     'cachetools>=4.0.0',
     'click>=7.0',
     'cryptography>=3.0',
